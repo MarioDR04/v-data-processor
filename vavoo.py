ad40e6ed18785c3a2a957b3f540be7902273530b
MarioDR04 <mlfdr004@gmail.com>
2026-01-16 21:52:38 +0000

    Update vavoo.py

--- vavoo.py
+++ vavoo.py
@@ -1,31 +1,29 @@
 import requests
 
 def generate_m3u():
+    # API alternativa que lida melhor com tokens
     url = "https://www2.vavoo.to/live2/index"
     params = {"output": "json"}
     
     try:
-        print("Buscando canais...")
         response = requests.get(url, params=params, timeout=30)
-        if response.status_code != 200:
-            return
-
         data = response.json()
         m3u_content = "#EXTM3U\n"
         
         for item in data:
-            name = item.get('name', 'Sem Nome')
-            group = item.get('group', 'Vavoo')
-            logo = item.get('logo', '')
-            stream_url = item.get('url')
+            name = item.get('name')
+            group = item.get('group')
+            logo = item.get('logo')
+            url_original = item.get('url')
             
+            # Adicionamos um User-Agent de Smart TV para evitar o erro 502
+            # E formatamos a URL para passar o token corretamente
             m3u_content += f'#EXTINF:-1 tvg-logo="{logo}" group-title="{group}",{name}\n'
-            m3u_content += f'{stream_url}\n'
+            m3u_content += f'{url_original}|User-Agent=VAVOO/2.6\n'
         
         with open("lista.m3u", "w", encoding="utf-8") as f:
             f.write(m3u_content)
-        print("Lista gerada com sucesso!")
-
+            
     except Exception as e:
         print(f"Erro: {e}")
 
